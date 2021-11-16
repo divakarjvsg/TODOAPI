f6d7c842a433f80c44c693e5c281a28da25ebab4
Divakar Jalumuri <divakarjvsg@gmail.com>
2021-11-16 15:40:04 +0530

    updating unit test cases

--- TodoApi-tests/LabelsControllerTest.cs
+++ TodoApi-tests/LabelsControllerTest.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TodoAPI.Controllers;
+using TodoAPI.Interfaces;
 using TodoAPI.Models;
 using TodoAPI.Repositories;
 using Xunit;
@@ -12,15 +15,19 @@ namespace TodoApi_tests
     public class LabelsControllerTest:ControllerBase
     {
         private readonly LabelsController _labelsController;
-        private readonly ILabelRepository _labelRepository;
+        
         public LabelsControllerTest()
         {
-            _labelRepository = new LabelServiceFake();
-            _labelsController = new LabelsController(_labelRepository,null,null,null);
+            var labelRepositorystub = new Mock<ILabelRepository>();
+            var todolistrepositoryStub = new Mock<ITodoListRepository>();
+            var todoitemrepositoryStub = new Mock<ITodoItemsRepository>();
+            var loggerStub = new Mock<ILogger<LabelsController>>();
+
+            _labelsController = new LabelsController(labelRepositorystub.Object, todoitemrepositoryStub.Object, todolistrepositoryStub.Object, loggerStub.Object);
         }
 
         [Fact]
-        public void AssignLabel_ValidObjectPassed_ReturnsCreatedResponse() {
+        public async Task  AssignLabel_ValidObjectPassed_ReturnsNoContentResponse() {
 
             // Arrange
             var SelectedGuid = new Guid("2da94280-7259-4fdb-bb2d-b0aae146a16b");
@@ -28,9 +35,29 @@ namespace TodoApi_tests
             
             SelectedLabels.Add(new Labels { LabelId=1003});
             SelectedLabels.Add(new Labels { LabelId = 4 });
+            //Act
+            var result= await _labelsController.AssignLabels(SelectedGuid, SelectedLabels);
+            
+            //Assert
+            Assert.IsType<ObjectResult>(result);
+        }
 
-            var result= _labelsController.AssignLabels(SelectedGuid, SelectedLabels);
-            Assert.IsType<CreatedAtActionResult>(result);
+
+        [Fact]
+        public async Task AssignLabel_ValidObjectPassed_ReturnsCreatedResponse()
+        {
+
+            // Arrange
+            var SelectedGuid = new Guid("230feef2-1065-4f4b-ba95-965907fe5916");
+            List<Labels> SelectedLabels = new List<Labels>();
+
+            SelectedLabels.Add(new Labels { LabelId = 1003 });
+            SelectedLabels.Add(new Labels { LabelId = 4 });
+            //Act
+            var result = await _labelsController.AssignLabels(SelectedGuid, SelectedLabels);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
